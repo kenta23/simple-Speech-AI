b525c67f0374fdc04260cf3c4e5d425c2416a933
= <dreyx0159@gmail.com>
2024-04-17 11:12:53 +0800

    ,,,

--- app/api/testing/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { NextResponse } from "next/server";
-
-export async function GET(req, res:NextApiResponse) {
-
-  return NextResponse.json({ message: 'Hello from Next.js API!', data: 'Some data' }); // Set appropriate status code and response data
-}
